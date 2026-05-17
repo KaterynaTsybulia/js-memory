75a643dd3966dbd4b972af39bfbe14d381065d8f
Katerina <ekaterina041117@gmail.com>
2026-05-17 18:05:03 +0300

    test hour

--- js/task1.js
+++ js/task1.js
@@ -1,7 +1,24 @@
-function makeTransaction(quantity, pricePerDroid) {
-	//
+// function getTime(minutes) {
+//   const hour = Number.parseInt(minutes / 60);
+//   const minutesOne = minutes % 60;
+//   const totalMinutes = `${hour}:${minutesOne}`;
+//   return totalMinutes;
+// }
+
+// function getTime(minutes) {
+//   const hour = Number.parseInt(minutes / 60);
+//   const minutesOne = minutes % 60;
+//   return `${hour.toString().padStart(2, '0')}:${minutesOne.toString().padStart(2, '0')}`;
+// }
+
+function getTime(minutes) {
+  const hour = Math.floor(minutes / 60);
+  const minutesOne = minutes % 60;
+  return `${hour.toString().padStart(2, '0')}:${minutesOne.toString().padStart(2, '0')}`;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(getTime(5)); // 00:05
+console.log(getTime(65)); // 01:05
+console.log(getTime(70)); // 01:10
+console.log(getTime(450)); // 07:30
+console.log(getTime(1441)); // 24:01
