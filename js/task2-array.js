46ca68752795f97bf84dd53e93d0ce5d8987dab4
Katerina <ekaterina041117@gmail.com>
2026-06-11 22:02:37 +0300

    array

---
+++ js/task2-array.js
@@ -0,0 +1,46 @@
+// const array = ['Hello world', null, true, 15, false, 98, 3];
+// console.log(array);
+// const firstEl = array[0];
+// console.log(firstEl);
+// const lastInd = array.length - 1;
+// console.log(lastInd);
+// const lastEl = array[lastInd];
+// console.log(lastEl);
+
+// const arrayJoin = array.join('+');
+// console.log(arrayJoin);
+
+// const arraySplit = firstEl.split('');
+// console.log(arraySplit);
+
+// const arrayJoinSp = arraySplit.join('');
+// console.log(arrayJoinSp);
+
+// const arraySlice = array.slice(1, 6);
+// console.log(arraySlice);
+
+// for (let i = 0; i < array.length; i += 1) {
+//   const item = array[i];
+//   console.log(item);
+// }
+
+// for (const item of array) {
+//   console.log(item);
+// }
+
+// const a = [15, 9, 3];
+
+// a[0] = 6;
+// console.log(a);
+
+// a.push(4, 7, 9);
+// console.log(a); //(6) [15, 9, 3, 4, 7, 9]
+// console.log(a.push(5, 8, 3)); // 9
+
+// a.pop();
+// console.log(a); // (8) [15, 9, 3, 4, 7, 9, 5, 8]
+// a.pop();
+// console.log(a); // (7) [15, 9, 3, 4, 7, 9, 5]
+
+// console.log(a.unshift(0));
+// console.log(a.shift());
